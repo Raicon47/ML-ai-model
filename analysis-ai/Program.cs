26f3a07db1423f7da1f1639e23f1bd3d6fb1aab6
Cyprian Nwaerema <icondikenabia@yahoo.com>
2023-08-13 06:18:36 -0700

    first commit

---
+++ analysis-ai/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Load sample data
+using Analysis_ai;
+
+var sampleData = new MLModel1.ModelInput()
+{
+    Col0 = @"disgusting.",
+};
+
+//Load model and predict output
+var result = MLModel1.Predict(sampleData);
+
+// If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
